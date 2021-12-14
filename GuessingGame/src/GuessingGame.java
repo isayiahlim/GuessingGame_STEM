ecc6af035ee173c2fc036fdb7b256d8bc7926eb7
s-ILIM <s-ILIM@487M113743.lwsd.k12wa.org>
2021-12-14 08:53:09 -0800

    First commit after adding the header!

--- GuessingGame/src/GuessingGame.java
+++ GuessingGame/src/GuessingGame.java
@@ -1,13 +1,14 @@
 /*
- * Name
- * Date
- * APCS Period
+ * Name: Isayiah Lim
+ * Date: 12/14/2021
+ * APCS Period 2
  * Guessing Game Project
  * 
- * Program Description
+ * This program makes a guessing game where the use tries to guess a random number between 1-100.
+ * The user can play it again after they win. After they stop playing, the program displays stats
+ * telling them info about their games. 
  */
 import java.util.Scanner;
-
 public class GuessingGame 
 {
 	public static void main(String[] args) 
