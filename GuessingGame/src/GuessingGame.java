a4565524aeacb2ce55a80307413974786ebb0c90
TeslaSTEMCSTeacher <bkankelborg@lwsd.org>
2021-12-14 08:32:56 -0800

    Populating starter project: Part 2

---
+++ GuessingGame/src/GuessingGame.java
@@ -0,0 +1,17 @@
+/*
+ * Name
+ * Date
+ * APCS Period
+ * Guessing Game Project
+ * 
+ * Program Description
+ */
+import java.util.Scanner;
+
+public class GuessingGame 
+{
+	public static void main(String[] args) 
+	{
+		
+	}
+}
