59b943d92d74d8f52957933661165abf86975daf
s-ILIM <s-ILIM@487M113743.lwsd.k12wa.org>
2021-12-15 15:26:02 -0800

    This is my code after working for half an hour. I wrote the methods.

--- GuessingGame/src/GuessingGame.java
+++ GuessingGame/src/GuessingGame.java
@@ -9,7 +9,6 @@
  * telling them info about their games. 
  */
 import java.util.Scanner;
-public final int MAX = 100;
 public class GuessingGame 
 {
 	public static void main(String[] args) 
@@ -18,48 +17,114 @@ public class GuessingGame
 		int totalGames = 0;
 		int totalGuesses = 0;
 		int bestGame = 0;
-
+		int guessCount = 0;
+		//variable that will run the game until made false
+		boolean gamePlay = true;
 		Scanner input = new Scanner(System.in);
 		
 		//prints out the intro message
 		System.out.println("Let's play a game!");
 		System.out.println();
 		
-		//starts the game
-		int guessNum = playGame();
-
-		//adds to the total statistics
-		totalGuesses += guessNum;
-		totalGames ++;
-		
-		//checks if the most recent game was the best
-		bestGame = bestGameCheck(guessNum, bestGame);
-		
-		playAgain(guessNum, input);
+		//starts the game and runs it until the player wants to stop
+		while(gamePlay)
+		{
+			guessCount = playGame(input);
+			//adds to the total statistics
+			totalGuesses += guessCount;
+			totalGames ++;
+			
+			//checks if the most recent game was the best
+			bestGame = bestGameCheck(guessCount, bestGame);
+				
+			//asks if the user wants to play again. If not, then it prints out the statistics
+			gamePlay = playAgain(input);
+		}
+		//gets and prints out the stats of the game
+		stats(guessCount, totalGames, totalGuesses, bestGame);	
 	}
 	
 	//plays game
-	public static int playGame()
+	public static int playGame(Scanner input)
 	{
-		int guessNum = 0;
+		//declares and initializes all the variables
+		int guessCount = 0;
+		int guessNum = (int)(Math.random()*100) + 1;
+		int guess = 0;
+		boolean isCorrect = false;
+		
+		//starts playing
 		System.out.println("I am thinking of a number between 1 and 100. Can you guess what it is?");
-		return guessNum;
+		System.out.println();
+		
+		//loop until the correct guess is made
+		while(!isCorrect)
+		{
+			System.out.print("Your guess? ");
+			guess = input.nextInt();
+			//checks if the number is right
+			isCorrect = (guess == guessNum);
+			//prints out whether it is higher or lower
+			if(guess < guessNum)
+				System.out.println("It's higher.");
+			else if (guess > guessNum)
+				System.out.println("It's lower.");
+			guessCount ++;
+		}
+		if (guessCount > 1)
+			System.out.println("You got it right in "+ guessCount + " guesses");
+		else
+			System.out.println("You got it right in 1 guess");
+		return guessCount;
 	}
-	
+		
 	//sees if the recent game was the lowest
 	public static int bestGameCheck(int guessNum, int bestGame)
 	{
 		if(guessNum < bestGame)
 			return guessNum;
+		return bestGame;
 	}
+	
+	//adds up the statistics
+	public static void stats(int guessCount, int totalGames, int totalGuesses, int bestGame)
 	{
-		
+		System.out.println("Overall Results:");
+		System.out.println("	Total Games     = "+ totalGames);
+		System.out.println("	Total Guesses   = " + totalGuesses);
+		System.out.println("    Average Guesses = " + (double)(totalGuesses)/totalGames);
+		System.out.println();
+		System.out.println("    Best Game       = " + bestGame);
 	}
 	
 	//checks if the user wants to play again
-	public static int playAgain(int guessNum, Scanner input)
+	public static boolean playAgain(Scanner input)
 	{
+		System.out.print("Do you wish to play again? ");
+		String answer = input.next();
+		answer = answer.substring(0,1);
+		answer = answer.toUpperCase();
+		//asks for a response until a viable one (starting with Y or N) is given
+		while(!answer.equals("Y") && !answer.equals("N"))
+		{
+			System.out.println("Sorry I didn't catch that.");
+			System.out.print("Do you wish to play again? ");
+			answer = input.next().substring(0,1).toUpperCase();
+		}
 		
+		//returns whether the player wants to continue
+		if(answer.equals("Y"))
+		{	
+			System.out.println();
+			return true;
+		}
+		else if(answer.equals("N"))
+		{
+			System.out.println();
+			return false;	
+		}
+		//void return statement that never gets used but eclipse is picky about
+		return true;
 	}
 	
 }
