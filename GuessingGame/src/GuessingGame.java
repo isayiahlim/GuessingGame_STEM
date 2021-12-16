630ce827c5aa27b37e7ec8173b2704db4c9d8371
s-ILIM <s-ILIM@487M113743.lwsd.k12wa.org>
2021-12-16 09:11:04 -0800

    Added a global variable that allows changing highest number in the range

--- GuessingGame/src/GuessingGame.java
+++ GuessingGame/src/GuessingGame.java
@@ -11,13 +11,14 @@
 import java.util.Scanner;
 public class GuessingGame 
 {
+	public static int MaxNum = 100;
 	public static void main(String[] args) 
 	{
 		//makes variables that are used for the statistics
 		int totalGames = 0;
 		int totalGuesses = 0;
 		//default number to ensure the code runs
-		int bestGame = 100;
+		int bestGame = 10000000;
 		int guessCount = 0;
 		//variable that will run the game until made false
 		boolean gamePlay = true;
@@ -36,7 +37,7 @@ public class GuessingGame
 			totalGames ++;
 			
 			//checks if the most recent game was the best
-			bestGame = bestGameCheck(guessCount, bestGame);
+			bestGame = Math.min(bestGame, guessCount);
 				
 			//asks if the user wants to play again. If not, then it prints out the statistics
 			gamePlay = playAgain(input);
@@ -50,12 +51,12 @@ public class GuessingGame
 	{
 		//declares and initializes all the variables
 		int guessCount = 0;
-		int guessNum = (int)(Math.random()*100) + 1;
+		int guessNum = (int)(Math.random()*MaxNum) + 1;
 		int guess = 0;
 		boolean isCorrect = false;
 		
 		//starts playing
-		System.out.println("I am thinking of a number between 1 and 100. Can you guess what it is?");
+		System.out.println("I am thinking of a number between 1 and " + MaxNum + ". Can you guess what it is?");
 		System.out.println();
 		
 		//loop until the correct guess is made
@@ -78,22 +79,17 @@ public class GuessingGame
 			System.out.println("You got it right in 1 guess");
 		return guessCount;
 	}
-		
-	//sees if the recent game was the lowest
-	public static int bestGameCheck(int guessNum, int bestGame)
-	{
-		if(guessNum < bestGame)
-			return guessNum;
-		return bestGame;
-	}
 	
 	//adds up the statistics
 	public static void stats(int guessCount, double totalGames, double totalGuesses, int bestGame)
 	{
+		//calculates the average number of guesses
 		double average = totalGuesses/totalGames;
+		//prints out all the results
 		System.out.println("Overall Results:");
 		System.out.println("	Total Games     = "+ (int)totalGames);
 		System.out.println("	Total Guesses   = " + (int)totalGuesses);
+		//prints out the average number of guesses to two decimal points
 		System.out.printf("	Average Guesses = %.2f", average);
 		System.out.println();
 		System.out.println("	Best Game       = " + bestGame);
